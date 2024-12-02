66bf9b3fa47eb9e935601e1967aa90bfc667b767
Jean-Étienne <git@jeanetienne.net>
2024-12-02 18:11:04 +0100

    Added a screen to show all tabs

--- OrionChallenge/AppDelegate.swift
+++ OrionChallenge/AppDelegate.swift
@@ -13,7 +13,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow(frame: UIScreen.main.bounds)
 
-        window.rootViewController = UINavigationController(rootViewController: TabsCollectionViewController(nibName: nil, bundle: nil))
+        let navigationController = UINavigationController(rootViewController: TabsCollectionViewController(viewModel: TabsCollectionViewModel()))
+        navigationController.setNavigationBarHidden(true, animated: false)
+        window.rootViewController = navigationController
 
         window.makeKeyAndVisible()
         self.window = window
---
+++ OrionChallenge/TabsCollectionView/Subviews/BrowserTabCollectionViewCell.swift
@@ -0,0 +1,61 @@
+//
+// OrionChallenge
+// Copyright © 2024 Jean-Étienne Parrot. All rights reserved.
+//
+
+import UIKit
+
+class BrowserTabCollectionViewCell: UICollectionViewCell {
+
+    static let nib = UINib(nibName: "BrowserTabCollectionViewCell", bundle: nil)
+    static let reuseIdentifier = "BrowserTabCollectionViewCell"
+
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var imageContainer: UIView!
+
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var iconImageView: UIImageView!
+
+    @IBOutlet private weak var closeButton: UIButton!
+
+    private var imageViewAspectRatioConstraint: NSLayoutConstraint?
+    private var closeAction: (() -> Void)?
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        imageContainer.layer.cornerRadius = 16
+        imageContainer.layer.cornerCurve = .continuous
+        imageContainer.clipsToBounds = true
+        iconImageView.image = UIImage(systemName: "globe")
+        closeButton.setTitle(nil, for: .normal)
+        closeButton.setImage(UIImage(named: "saltire_circle"), for: .normal)
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+
+        imageView.image = nil
+        imageViewAspectRatioConstraint?.isActive = false
+        imageViewAspectRatioConstraint = nil
+        titleLabel.text = nil
+        iconImageView.image = UIImage(systemName: "globe")
+        closeAction = nil
+    }
+
+    func configure(image: UIImage, title: String, icon: UIImage?, closeAction: @escaping (() -> Void)) {
+        imageView.image = image
+        imageViewAspectRatioConstraint = imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: image.size.height / image.size.width)
+        imageViewAspectRatioConstraint?.isActive = true
+        titleLabel.text = title
+        if let icon {
+            iconImageView.image = icon
+        }
+        self.closeAction = closeAction
+    }
+
+    @IBAction func closeButtonDidTouchUpInside(_ sender: UIButton, forEvent event: UIEvent) {
+        closeAction?()
+    }
+
+}
